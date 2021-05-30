f96f19193c52e9dd9063950ce43b23da2ada7dae
srivello <srivello@hotmail.com>
2021-05-30 12:28:56 -0600

    Add Store Data

--- client/Assets/Scripts/Runtime/Beamable/Samples/KOR/StoreSceneManager.cs
+++ client/Assets/Scripts/Runtime/Beamable/Samples/KOR/StoreSceneManager.cs
@@ -1,4 +1,8 @@
-﻿using Beamable.Api.Payments;
+﻿using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Beamable.Api.Payments;
+using Beamable.Common.Api;
 using Beamable.Common.Api.Inventory;
 using Beamable.Samples.KOR.Data;
 using Beamable.Samples.KOR.UI;
@@ -12,6 +16,9 @@ namespace Beamable.Samples.KOR
    /// </summary>
    public class StoreSceneManager : MonoBehaviour
    {
+      //  Consts ---------------------------------------
+      private const string ContentType = "items";
+      
       //  Fields ---------------------------------------
       [SerializeField]
       private Configuration _configuration = null;
@@ -22,13 +29,14 @@ namespace Beamable.Samples.KOR
       private IBeamableAPI _beamableAPI = null;
       private InventoryView _inventoryView = null;
       private PlayerStoreView _playerStoreView = null;
-
+      private List<string> _inventoryItems = new List<string>();
+      private List<string> _storeItems = new List<string>();
+      
       //  Unity Methods   ------------------------------
       protected void Start()
       {
          _storeUIView.BuyButton.onClick.AddListener(BuyButton_OnClicked);
          _storeUIView.BackButton.onClick.AddListener(BackButton_OnClicked);
-         
          SetupBeamable();
       }
 
@@ -39,22 +47,11 @@ namespace Beamable.Samples.KOR
             TMP_BufferedText.BufferedTextMode.Queue);
 
          _beamableAPI= await Beamable.API.Instance;
-
-         _beamableAPI.InventoryService.Subscribe(InventoryService_OnChanged);
-         _beamableAPI.CommerceService.Subscribe(CommerceService_OnChanged);
          LoadServices();
          
       }
       
-      private void DebugLog(string message)
-      {
-         if (_configuration.IsDebugLog)
-         {
-            Debug.Log(message);
-         }
-      }
-      
-      private async void LoadServices()
+      private void LoadServices()
       {
          _inventoryView = null;
          _playerStoreView = null;
@@ -62,31 +59,69 @@ namespace Beamable.Samples.KOR
          // Set loading text
          _storeUIView.BufferedText.SetText(KORConstants.StoreUIView_Loading_Store, 
             TMP_BufferedText.BufferedTextMode.Immediate);
-         _storeUIView.InventoryPanelUIView.BodyText.text = KORConstants.StoreUIView_Loading_Store;
+         _storeUIView.InventoryPanelUIView.BodyText.text = KORConstants.StoreUIView_Loading_Inventory;
          _storeUIView.StorePanelUIView.BodyText.text = KORConstants.StoreUIView_Loading_Store;
 
          // Reload the services
-         InventoryView inventoryView = await _beamableAPI.InventoryService.GetCurrent();
-         InventoryService_OnChanged(inventoryView);
-         
-         PlayerStoreView playerStoreView = await _beamableAPI.CommerceService.GetCurrent();
-         CommerceService_OnChanged(playerStoreView);
+         _beamableAPI.InventoryService.Subscribe(ContentType, InventoryService_OnChanged);
+         _beamableAPI.CommerceService.Subscribe(CommerceService_OnChanged);
+
       }
       
       private void CheckLoadServicesStatus()
       {
-         if (_inventoryView != null && _playerStoreView != null)
+         if (_inventoryView == null || _playerStoreView == null)
          {
-            //
-            _storeUIView.BufferedText.SetText(KORConstants.StoreUIView_Instructions, 
-               TMP_BufferedText.BufferedTextMode.Queue);
+            //TODO: Wait for both to load
+            //return;
          }
+         
+         _storeUIView.BufferedText.SetText(KORConstants.StoreUIView_Instructions, 
+            TMP_BufferedText.BufferedTextMode.Queue);
+
+         // Render inventory
+         StringBuilder inventoryStringBuilder = new StringBuilder();
+         foreach (var item in _inventoryItems)
+         {
+            inventoryStringBuilder.AppendLine($"•{item}").AppendLine();
+         }
+         _storeUIView.InventoryPanelUIView.BodyText.text = inventoryStringBuilder.ToString();
+
+         // Render store
+         StringBuilder storeStringBuilder = new StringBuilder();
+         foreach (var item in _inventoryItems)
+         {
+            storeStringBuilder.AppendLine($"•{item}").AppendLine();
+         }  
+         _storeUIView.StorePanelUIView.BodyText.text = storeStringBuilder.ToString();
       }
 
+      private void DebugLog(string message)
+      {
+         if (_configuration.IsDebugLog)
+         {
+            Debug.Log(message);
+         }
+      }
       //  Event Handlers -------------------------------
       private void InventoryService_OnChanged(InventoryView inventoryView)
       {
          _inventoryView = inventoryView;
+         
+         foreach (KeyValuePair<string, List<ItemView>> kvp in _inventoryView.items)
+         {
+            // User can have multiple of EACH item. Show simpler info here
+            foreach (ItemView itemView in kvp.Value)
+            {
+               int itemCount = kvp.Value.Count;
+               string itemName = kvp.Key.Replace("items.", "");
+               string itemDisplayName = $"\t{itemName} ({itemCount})";
+               
+               //TODO: Replace List<string> with List<blah> to hold more data?
+               _inventoryItems.Add(itemDisplayName);
+            }
+         }
+         
          Debug.Log($"InventoryService_OnChanged() _inventoryView = {_inventoryView}");
          CheckLoadServicesStatus();
       }
