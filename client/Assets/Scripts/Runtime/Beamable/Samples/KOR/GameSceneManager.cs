1845aa3dce9e7e177b4a55116eef00175e90ee5c
VerticalLongboard <73481831+VerticalLongboard@users.noreply.github.com>
2021-06-10 15:51:52 -0500

    Added smoke trails to characters

--- client/Assets/Scripts/Runtime/Beamable/Samples/KOR/GameSceneManager.cs
+++ client/Assets/Scripts/Runtime/Beamable/Samples/KOR/GameSceneManager.cs
@@ -253,15 +253,6 @@ namespace Beamable.Samples.KOR
             }
         }
 
-        public void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Debug.Log("TODO: Remove this method");
-                _gameUIView.CinemachineImpulseSource.GenerateImpulse();
-            }
-        }
-
         public void ShakeCamera(float force=1)
         {
             _gameUIView.CinemachineImpulseSource.GenerateImpulse();
